c48095fa7a66e92bbf6f31b7459f0be4ee4f342d
mertbolukbasi <kaizenium@kaizeniumfoundation.com>
2025-12-21 02:08:09 +0300

    Database connection errors was fixed

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,14 @@ module com.group16.grocery_app {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-
+    requires mysql.connector.j;
     requires MaterialFX;
     requires org.controlsfx.controls;
 
-    opens com.group16.grocery_app.controller to javafx.fxml;
     opens com.group16.grocery_app to javafx.fxml;
+
+    opens com.group16.grocery_app.controller to javafx.fxml;
+    opens com.group16.grocery_app.model to javafx.base;
+
     exports com.group16.grocery_app;
 }
